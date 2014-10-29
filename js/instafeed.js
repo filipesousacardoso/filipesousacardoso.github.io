758bb591e4ced6c0e26c63ac5f21af2e7b2e94ff
Filipe Sousa Veloso de Oliveira Cardoso <filipecardoso.88@gmail.com>
2014-10-29 12:57:03 +0000

    ss

--- js/instafeed.js
+++ js/instafeed.js
@@ -174,7 +174,7 @@
             image = images[_k];
 			
 			var instacell=instacells[_k];
-			instacell.style.backgroundImage="url(../img/pena1.jpg)";
+			instacell.style.backgroundImage="url(http://4.bp.blogspot.com/-BrLngyC1Bto/UpO-IY1kcNI/AAAAAAAALqA/1bSQRy3G_gU/s1600/Image-b%C3%A9b%C3%A9-facebook-8.jpg)";
 			
             img = document.createElement('img');
             imageUrl = image.images[this.options.resolution].url;
