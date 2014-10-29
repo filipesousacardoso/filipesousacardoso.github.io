954d6e074b019d307a8763cac7384f32b0b6afb9
Filipe Sousa Veloso de Oliveira Cardoso <filipecardoso.88@gmail.com>
2014-10-29 12:29:13 +0000

    insta

--- js/instafeeddd.js
+++ js/instafeeddd.js
@@ -9,13 +9,14 @@
       this.options = {
         target: 'instafeed',
         get: 'popular',
-        resolution: 'thumbnail',
-        sortBy: 'none',
-        links: true,
+        resolution: 'low_resolution',
+        sortBy: 'most-recent',
+        links: false,
         mock: false,
         useHttp: false,
-		limit:6
+		limit: 9
       };
+	  
       if (typeof params === 'object') {
         for (option in params) {
           value = params[option];
@@ -165,10 +166,18 @@
             fragment.appendChild(node);
           }
         } else {
-          for (_k = 0, _len2 = images.length; _k < _len2; _k++) {
+			console.log("lets see");
+			var instacells = document.getElementsByClassName('instacell');
+			if(instacells!=null){console.log("not null");}
+          
+		  for (_k = 0, _len2 = images.length; _k < _len2; _k++) {
             image = images[_k];
             img = document.createElement('img');
             imageUrl = image.images[this.options.resolution].url;
+			
+			var instacell=instacells[_k];
+			instacell.style.background="white";		
+			
             if (!this.options.useHttp) {
               imageUrl = imageUrl.replace('http://', '//');
             }
